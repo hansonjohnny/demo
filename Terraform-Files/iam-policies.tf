91d7738afc9be22e85c8464b2ea84f0f70c14d37
johnny <hansonjohnny648@gmail.com>
2026-04-28 22:46:45 +0000

    updates

--- Terraform-Files/iam-policies.tf
+++ Terraform-Files/iam-policies.tf
@@ -78,3 +78,8 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_readonly" {
   role       = aws_iam_role.eks_nodes.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+resource "aws_iam_role_policy_attachment" "eks_ebs_csi" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
